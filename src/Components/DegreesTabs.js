5f7e08a6971f202c4744ab59c61677c849fbe504
Michael McIntosh <mjm6048@rit.edu>
2022-11-15 03:16:01 -0500

    Removed unused code

--- src/Components/DegreesTabs.js
+++ src/Components/DegreesTabs.js
@@ -3,18 +3,6 @@ import { Tab } from 'semantic-ui-react'
 import getData from '../util/getData';
 import Degrees from './DegreesUndergraduate';
 
-/*
-const panes = [
-    { menuItem: 'Tab 1', render: () => <Tab.Pane>Tab 1 Content</Tab.Pane> },
-    { menuItem: 'Tab 2', render: () => <Tab.Pane>Tab 2 Content</Tab.Pane> },
-    { menuItem: 'Tab 3', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> },
-]
-
-const TabExampleBasic = () => <Tab panes={panes} />
-
-export default TabExampleBasic
-*/
-
 export default class DegreesTabs extends React.Component {
     constructor(props) {
         super(props);
